3102dfbfe16baf1db7aeec402c1ca634b031d99a
John <johnsief12@gmail.com>
2024-04-13 14:11:15 -0400

    setting up click event

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // 1 meter = 3.281 feet
 // 1 liter = 0.264 gallon
 // 1 kilogram = 2.204 pound
+const convertBtn = document.querySelector("#convert-btn");
+const convertNum = document.querySelector(".convert-box").value
+
+function renderConvert(){
+
+
+}
+
+
+
+
+
+convertBtn.addEventListener("click", renderConvert)
